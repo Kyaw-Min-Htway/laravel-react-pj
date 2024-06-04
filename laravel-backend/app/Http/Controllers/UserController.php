f8cc347747f751477ce328bb17b5357a9771166a
KyawMinHtway066813 <batman066813@gmail.com>
2024-06-04 22:07:43 +0630

    first commit

---
+++ laravel-backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index(){
+        $users = User::all();
+
+        //return json response
+        return response()->json([
+            'results' => $users
+        ],200);
+    }
+
+    public function show($id){
+        //user detail
+        $users = User::find($id);
+        if(!$users){
+            return response()->json([
+                'message' => 'User not found!'
+            ],404);
+        }
+
+        return response()->json([
+            'users' => $users
+        ],200);
+    }
+}
